21a3dfbc27e39b2087286d6b5de1a1dee9bf4315
sugamypapa <sk000801@naver.com>
2022-10-25 15:42:15 +0900

    feat: 사용자 입력 수 예외처리 함수 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -14,15 +14,45 @@ public class Application {
         }
         return correctNum;
     }
-    public static int[] madeArr(int EnterNum) {
+
+    public static boolean checkCorrectNum(String enterNum) {
+        boolean setError = false;
+        if(enterNum.length() != 3) setError = true;
+        else if(enterNum.charAt(0) != enterNum.charAt(1)
+                && enterNum.charAt(1) != enterNum.charAt(2)
+                && enterNum.charAt(0) != enterNum.charAt(2)) setError = true;
+        return setError;
+    }
+
+    public static int[] madeArr(int enterNum) {
         // 사용자로부터 입력받은 수를 한자리씩 배열로 받아옴
-        return new int[]{EnterNum/100, (EnterNum/10)/10, EnterNum%10};
+        return new int[]{enterNum/100, (enterNum/10)/10, enterNum%10};
+    }
+
+    public static int findStrike(int[] arr1, int[] arr2) {
+        for(int i=0; i<3; i++) {
+            if(arr1[i] == arr2[i]) strikeNumber++;
+        }
+        return strikeNumber;
+    }
+
+    public static int findBall(int[] arr1, int[] arr2) {
+        for(int i=)
     }
 
     public static void main(String[] args) {
 
         correctNum = createCorrectNumber();
+
+        System.out.print("숫자를 입력해주세요 : ");
+        if(checkCorrectNum(Console.readLine())) {
+            throw new IllegalArgumentException();
+        }
         playerNum = madeArr(Integer.parseInt(Console.readLine()));
+        //여기 일단 깔끔하게 고쳐보기
+
+
+
 
     }
 }
