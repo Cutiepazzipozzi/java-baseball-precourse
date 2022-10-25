4c93d4b17b144e249cbbdbe5f4d1bfda0eb04a7d
sugamypapa <sk000801@naver.com>
2022-10-25 14:18:23 +0900

    feat: 맞힐 숫자 저장하는 함수

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,20 @@
 package baseball;
+import static camp.nextstep.edu.missionutils.Console.readLine;
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 
 public class Application {
+    public static int madeNumber(int num) {
+        for(int i=0; i<3; i++) {
+            num += Math.pow(10, 2-i)*pickNumberInRange(1,9);
+        }
+        return num;
+    }
     public static void main(String[] args) {
-        //TODO: 숫자 야구 게임 구현
 
+        int CorrectNum = 0;
+        int EnterNum = 0;
+
+        madeNumber(CorrectNum);
+        EnterNum = Integer.parseInt(readLine());
     }
 }
